eceeeb575769f9e7f6f95e2279e0c88a377bde00
jackmarley254 <engndiritu1@gmail.com>
2024-05-04 02:12:21 +0300

    Project solution files

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-"""Contains a single function that concatenates two strings."""
 
 
-def concat(str1: str, str2: str) -> str:
-    """Concatenates two strings."""
-    return str1 + str2
+#"""Contains a single function that concatenates two strings."""
+
+
+#def concat(str1: str, str2: str) -> str:
+##    """Concatenates two strings."""
+#    return str1 + str
+"""Module that contains a single function that adds two integers."""
+
+
+def add(a: float, b: float) -> float:
+    """ Add two integers together."""
+    return a + b
