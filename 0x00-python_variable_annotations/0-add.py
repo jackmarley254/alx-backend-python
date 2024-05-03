49ad729a4fd8a950229bd60c4d948f2956f25120
jackmarley254 <engndiritu1@gmail.com>
2024-05-04 01:38:31 +0300

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Module that contains a single function that adds two integers."""
+
+
 def add(a: float, b: float) -> float:
 	""" Add two integers together."""
-	sum = a+b
-	return sum
+	
+	return a + b
 
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
+#print(add(1.11, 2.22) == 1.11 + 2.22)
+#print(add.__annotations__)
 
